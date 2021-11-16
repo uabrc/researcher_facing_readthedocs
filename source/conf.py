b7fa5a4a06ba5c9459b22cca3f77c73aa1cdbdae
Matthew K Defenderfer <mdefende@uab.edu>
2021-11-16 16:50:59 -0600

    remove bibtex dependency

--- source/conf.py
+++ source/conf.py
@@ -61,8 +61,9 @@ author = "UAB Research Computing"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autosectionlabel", "sphinxcontrib.bibtex"]
-bibtex_bibfiles = ["_test/bibtex/mybib.bib"]
+extensions = ["sphinx.ext.autosectionlabel"]
+#extensions = ["sphinx.ext.autosectionlabel", "sphinxcontrib.bibtex"]
+#bibtex_bibfiles = ["_test/bibtex/mybib.bib"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
